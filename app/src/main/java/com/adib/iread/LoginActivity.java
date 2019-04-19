78fa76a6ca7283ccf21a8f6349e01c6c2fa05861
AMINE404 <amineadib007@gmail.com>
2019-04-19 12:02:59 -0700

    Initial commit

---
+++ app/src/main/java/com/adib/iread/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.adib.iread;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+//Search books,Search books, authors, publishers…
+public class LoginActivity extends AppCompatActivity {
+    TextView signAct;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        signAct = findViewById(R.id.log_signup);
+        signAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sign = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(sign);
+            }
+        });
+    }
+}
