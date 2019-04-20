3e753db2c0a89d4e2a4e056c7e0954b4af5fc162
boujnah <yassineboujnah@gmail.com>
2019-04-20 12:47:26 +0100

    yass

--- app/src/main/java/com/adib/iread/MainActivity.java
+++ app/src/main/java/com/adib/iread/MainActivity.java
@@ -1,5 +1,7 @@
 package com.adib.iread;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.configureViewPagerAndTabs();
+    }
+    private void configureViewPagerAndTabs(){
+        // 1 - Get ViewPager from layout
+        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
+        // 2 - Set Adapter PageAdapter and glue it together
+        pager.setAdapter(new com.example.viewpager.Adapters.PageAdapter(getSupportFragmentManager()));
+        TabLayout tabs= (TabLayout)findViewById(R.id.activity_main_tabs);
+        tabs.setupWithViewPager(pager);
+        tabs.setTabMode(TabLayout.MODE_FIXED);
+        for (int i = 0; i < tabs.getTabCount(); i++) {
+            tabs.getTabAt(i).setIcon(R.drawable.ic_search_white_24dp);
+            tabs.getTabAt(i).setText(null);
+        }
     }
 }
